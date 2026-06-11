d1adb6310ca461136d75db95394612fdc6d0b832
Baptiste Leroyer <baptiste.leroyer@ovhcloud.com>
2026-06-11 13:50:10 +0200

    feat: integration de l'analytics Umami via variables d'environnement dynamiques

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,14 +3,21 @@
 	import "../app.css";
 	import { page } from "$app/state";
 	import Footer from "$lib/components/Footer.svelte";
+	import { env } from "$env/dynamic/public";
 
 	let { children } = $props();
 
 	const showFooter = $derived(!page.url.pathname.startsWith("/table/"));
+
+	const umamiUrl = $derived(env.PUBLIC_UMAMI_URL);
+	const umamiId = $derived(env.PUBLIC_UMAMI_WEBSITE_ID);
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	{#if umamiUrl && umamiId}
+		<script defer src={umamiUrl} data-website-id={umamiId}></script>
+	{/if}
 </svelte:head>
 
 {#if showFooter}
